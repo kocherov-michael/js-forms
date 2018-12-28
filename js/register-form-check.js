1f7b4f7d7ad676ecebf4d5591d2176b961a6dfe3
Michael <ko4erov@mail.ru>
2018-12-28 21:01:56 +0300

    add errors in JS

--- js/register-form-check.js
+++ js/register-form-check.js
@@ -3,16 +3,15 @@ $(document).ready(function(){
 	var registrationFormCheck = (function(){
 
 		var email = 'mail@mail.com';
-		// var password = '123';
 
 		// Переменные модуля
 		var _form = $('#registration-form');
 		var _registrationEmail = $('#registration-email');
 		var _registrationPassword = $('#registration-password');
-		var _emailErrorEmpty = $('#registration-email-empty');
-		var _emailErrorWrong = $('#registration-email-wrong');
-		var _passwordErrorEmpty = $('#registration-password-empty');
-		var _emailErrorExist = $('#registration-email-exist');
+		var _emailErrorEmpty = $('<div class="notify mb-20 notify--error">Введите email</div>');
+		var _emailErrorWrong = $('<div id="registration-email-wrong" class="notify notify--error mb-20 notify-hide">Неверный формат email</div>');
+		var _passwordErrorEmpty = $('<div id="registration-password-empty" class="notify notify--error mb-20 notify-hide">Введите пароль</div>');
+		var _emailErrorExist = $('<div id="registration-email-exist" class="notify no-paddings notify-hide"><div class="notify no-radius-bottom notify--error">Данный email уже занят</div><div class="notify no-radius-top"><p>Используйте другой email чтобы создать новый аккаунт.</p><p> Или воспользуйтесь<a href="#!">восстановлением пароля </a>, чтобы войти на сайт.</p></div></div>');
 
 		// Метод инициализации (запуска) модуля
 		var init = function(){
@@ -39,19 +38,19 @@ $(document).ready(function(){
     		var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
     		if(pattern.test(_registrationEmail.val())) {
     			if (_registrationEmail.val().trim() == email) {
-    				_emailErrorExist.fadeIn(1000);
+    				_emailErrorExist.prependTo('#registration-form').fadeIn(1000);
     				event.preventDefault();
     			} else if (_registrationPassword.val().trim() == '') {
-    				_passwordErrorEmpty.fadeIn(1000);
+    				_passwordErrorEmpty.prependTo('#registration-form').fadeIn(1000);
     				event.preventDefault();
     			} else {
     				console.log('correct');
     			}
     		} else if (_registrationEmail.val().trim() == '') {
-    			_emailErrorEmpty.fadeIn(1000);
+    			_emailErrorEmpty.prependTo('#registration-form').fadeIn(1000);
     			event.preventDefault();
     		}else {
-    			_emailErrorWrong.fadeIn(1000);
+    			_emailErrorWrong.prependTo('#registration-form').fadeIn(1000);
     			event.preventDefault();
     		}
 		}
