df1ce513d78df821f4260e59781032269e747adb
Michael <ko4erov@mail.ru>
2018-12-29 23:06:28 +0300

    chahge line of checking errors

--- js/register-form-check.js
+++ js/register-form-check.js
@@ -9,9 +9,10 @@ $(document).ready(function(){
 		var _registrationEmail = $('#registration-email');
 		var _registrationPassword = $('#registration-password');
 		var _emailErrorEmpty = $('<div class="notify mb-20 notify--error">Введите email</div>');
-		var _emailErrorWrong = $('<div id="registration-email-wrong" class="notify notify--error mb-20 notify-hide">Неверный формат email</div>');
-		var _passwordErrorEmpty = $('<div id="registration-password-empty" class="notify notify--error mb-20 notify-hide">Введите пароль</div>');
-		var _emailErrorExist = $('<div id="registration-email-exist" class="notify no-paddings notify-hide"><div class="notify no-radius-bottom notify--error">Данный email уже занят</div><div class="notify no-radius-top"><p>Используйте другой email чтобы создать новый аккаунт.</p><p> Или воспользуйтесь<a href="#!">восстановлением пароля </a>, чтобы войти на сайт.</p></div></div>');
+		var _emailErrorWrong = $('<div class="notify notify--error mb-20 notify-hide">Неверный формат email</div>');
+		var _passwordErrorEmpty = $('<div class="notify notify--error mb-20 notify-hide">Введите пароль</div>');
+		var _emailErrorExist = $('<div class="notify no-paddings"><div class="notify no-radius-bottom notify--error">Данный email уже занят</div><div class="notify no-radius-top"><p>Используйте другой email чтобы создать новый аккаунт.</p><p> Или воспользуйтесь<a href="#!">восстановлением пароля </a>, чтобы войти на сайт.</p></div></div>');
+		var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
 
 		// Метод инициализации (запуска) модуля
 		var init = function(){
@@ -28,24 +29,20 @@ $(document).ready(function(){
 		}
 		// Убираем уведомления об ошибках
 		var _removeError = function(){
-			_emailErrorEmpty.fadeOut(0);
-			_emailErrorWrong.fadeOut(0);
-			_passwordErrorEmpty.fadeOut(0);
-			_emailErrorExist.fadeOut(0);
+			var _errorMessage = $('.notify').fadeOut(0);
 		}
 		// Проверяем вводимые данные
 		var _formLogin = function (event) {
-    		var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
     		if (_registrationEmail.val().trim() == '' && _registrationPassword.val().trim() == '') {
     			_passwordErrorEmpty.prependTo('#registration-form').fadeIn(1000);
     			_emailErrorEmpty.prependTo('#registration-form').fadeIn(1000);
     			event.preventDefault();
     		}else if(pattern.test(_registrationEmail.val())) {
-    			if (_registrationEmail.val().trim() == email) {
-    				_emailErrorExist.prependTo('#registration-form').fadeIn(1000);
-    				event.preventDefault();
-    			} else if (_registrationPassword.val().trim() == '') {
+    			if (_registrationPassword.val().trim() == '') {
     				_passwordErrorEmpty.prependTo('#registration-form').fadeIn(1000);
+    				event.preventDefault();
+    			} else if (_registrationEmail.val().trim() == email) {
+    				_emailErrorExist.prependTo('#registration-form').fadeIn(1000);
     				event.preventDefault();
     			} else {
     				console.log('correct');
